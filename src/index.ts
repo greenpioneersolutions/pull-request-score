6f362f37ead6fa4dce84ec263ac8cbe3a7c39dbe
Jason Humphrey <Humphrey.jason32@gmail.com>
2025-06-13 05:03:30 -0500

    Add extensive normalizer examples and tests

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { calculateReviewMetrics } from "./calculators/reviewMetrics.js";
 export { calculateMetrics } from "./calculators/metrics.js";
 export { calculateCiMetrics } from "./calculators/ciMetrics.js";
 export { runCli } from "./cli.js";
+export { scoreMetrics } from "./scoring.js";
+export { createRangeNormalizer } from "./normalize.js";
